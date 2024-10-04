2b056bdfb5c57dab00b9fc17a3e28243e0a56696
silvak <silvak.jeg@gmail.com>
2024-10-03 21:14:37 -0400

    changes: improved stylos for ticket details

--- src/pages/private/tickets/detail-ticket.page.tsx
+++ src/pages/private/tickets/detail-ticket.page.tsx
@@ -8,6 +8,7 @@ import { useHistoryStore, useProjectStore, useTicketStore } from '@/store'
 import { useEffect, useState } from 'react'
 import { FaSave } from 'react-icons/fa'
 import { LuUser2 } from 'react-icons/lu'
+import PhoneInput from 'react-phone-input-2'
 import { useParams } from 'react-router-dom'
 
 export const convertToDateFormat = (dateString: string) => {
@@ -45,57 +46,10 @@ export const DetailTicketPage = () => {
     <LayoutGrid>
       <div className="flex flex-col rounded-xl p-0 col-span-1 sm:col-span-2 md:col-span-6 xl:col-span-12">
         <h1 className="text-2xl font-semibold">Detalles del Ticket</h1>
-      </div>
 
-      {/* info / update */}
-      <div className="bg-white rounded-xl p-2 col-span-1 sm:col-span-2 md:col-span-6 xl:col-span-4">
-        <div className="flex justify-between items-center">
-          <div className="border w-min py-1 px-2 bg-slate-100 rounded-sm">
-            <h4 className="whitespace-nowrap">Info</h4>
-          </div>
-
-          <div className="h-[38px]">
-            <CustomModal
-              header={<h2 className="text-xl font-semibold">Detalles Ticket</h2>}
-              buttonText="Actualizar"
-              buttonType="update"
-              buttonIcon={<FaSave />}
-              autoClose={isOpen}
-            >
-              {selectedTicket && (
-                <UpdateTicketForm
-                  ticket={selectedTicket}
-                  modalAutoClose={modalAutoClose}
-                />
-              )}
-            </CustomModal>
-          </div>
-        </div>
-
-        <div className="grid grid-cols-1 mt-3 gap-1 text-sm">
-          <p>
-            <strong>Nombre</strong> {selectedTicket.ownerData.name}
-          </p>
-          <p>
-            <strong>CI</strong> {selectedTicket.ownerData.dni}
-          </p>
-          <p>
-            <strong>Teléfono 1</strong> {selectedTicket.ownerData.phone1}
-          </p>
-          <p>
-            <strong>Teléfono 2</strong> {selectedTicket.ownerData.phone1}
-          </p>
-          <p>
-            <strong>Direccion</strong> {selectedTicket.ownerData.address}
-          </p>
-          <p>
-            <strong>Otro </strong> {selectedTicket.ownerData.other}
-          </p>
-        </div>
-
-        <div className="flex items-center gap-2 py-2 mt-3">
-          <p>Vendido por:</p>
-          <div className="flex items-center gap-2 pl-2 pr-4 p-1 border w-min rounded-full">
+        <div className="flex items-center gap-2 py-2">
+          <p className="font-semibold">Vendido por</p>
+          <div className="flex items-center gap-2 pl-2 pr-4 p-1 border w-min rounded-full bg-white">
             <div className="bg-slate-700 h-[32px] w-[32px] rounded-full text-white flex justify-center items-center">
               <LuUser2 />
             </div>
@@ -104,6 +58,99 @@ export const DetailTicketPage = () => {
               {selectedTicket.seller?.name ?? 'Sin vendedor'}
             </p>
           </div>
+        </div>
+      </div>
+
+      {/* info / update */}
+      <div className="bg-white rounded-xl p-2 col-span-1 sm:col-span-2 md:col-span-6 xl:col-span-4">
+        <div className="flex justify-between items-center">
+          <div className="border w-min py-1 px-2 bg-slate-100 rounded-sm">
+            <h4 className="whitespace-nowrap">Información</h4>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 mt-3 gap-1 text-sm">
+          <div className="flex items-center gap-2">
+            <label className="text-sm font-medium w-[80px]">Nombre</label>
+            <input
+              type="text"
+              name="name"
+              value={selectedTicket.ownerData.name}
+              className="w-full mt-1 p-2 border border-gray-300 rounded"
+              disabled
+            />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <label className="text-sm font-medium w-[80px]">CI-DNI</label>
+            <input
+              type="text"
+              name="dni"
+              value={selectedTicket.ownerData.dni}
+              className="w-full mt-1 p-2 border border-gray-300 rounded"
+              disabled
+            />
+          </div>
+
+          <div className="">
+            <PhoneInput
+              country={'ve'}
+              value={selectedTicket.ownerData.phone1}
+              inputClass="phoneInput"
+              inputProps={{
+                name: 'phone1',
+              }}
+              disabled
+            />
+          </div>
+
+          <div className="">
+            <PhoneInput
+              country={'ve'}
+              value={selectedTicket.ownerData.phone2}
+              inputClass="phoneInput"
+              inputProps={{
+                name: 'phone2',
+              }}
+              disabled
+            />
+          </div>
+
+          <div className="flex gap-3 items-center">
+            <label className=" text-sm font-medium w-[80px]">Dirección</label>
+            <input
+              type="text"
+              name="address"
+              value={selectedTicket.ownerData.address}
+              className="w-full mt-1 p-2 border border-gray-300 rounded"
+              disabled
+            />
+          </div>
+
+          <div className="flex gap-3 items-center">
+            <label className=" text-sm font-medium w-[80px]">Otro</label>
+            <input
+              type="textarea"
+              name="other"
+              value={selectedTicket.ownerData.other}
+              className="w-full mt-1 p-2 border border-gray-300 rounded"
+              disabled
+            />
+          </div>
+        </div>
+
+        <div className="h-[38px] mt-2">
+          <CustomModal
+            header={<h2 className="text-xl font-semibold">Detalles Ticket</h2>}
+            buttonText="Actualizar"
+            buttonType="update"
+            buttonIcon={<FaSave />}
+            autoClose={isOpen}
+          >
+            {selectedTicket && (
+              <UpdateTicketForm ticket={selectedTicket} modalAutoClose={modalAutoClose} />
+            )}
+          </CustomModal>
         </div>
       </div>
 
@@ -170,9 +217,27 @@ export const DetailTicketPage = () => {
           <h4 className="whitespace-nowrap">Fecha limite</h4>
         </div>
 
-        <p className="border py-1 px-2 bg-slate-100 rounded-sm mt-3">
-          📅{convertToDateFormat(selectedTicket.date)} ➤ 📅{selectedProject?.date.end}
-        </p>
+        <div className="flex items-center gap-3 mt-3">
+          <div>
+            <label className=" text-sm font-medium w-[80px] mr-2">Inicio</label>
+            <input
+              type="date"
+              value={convertToDateFormat(selectedTicket.date)}
+              className="bg-inherit h-[40px] border px-2 rounded-md bg-slate-100"
+              disabled
+            />
+          </div>
+          ➤
+          <div>
+            <label className=" text-sm font-medium w-[80px] mr-2">Fin</label>
+            <input
+              type="date"
+              value={selectedProject?.date.end}
+              className="bg-inherit h-[40px] border px-2 rounded-md bg-slate-100"
+              disabled
+            />
+          </div>
+        </div>
       </div>
 
       <div className="mt-6 col-span-1 sm:col-span-2 md:col-span-6 xl:col-span-12">
