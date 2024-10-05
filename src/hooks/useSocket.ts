485a6112f5830ca7101f030a8ff6135577cb85dd
silvak <silvak.jeg@gmail.com>
2024-10-05 12:50:23 -0400

    fix: url socket

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,39 +1,39 @@
-import { useEffect, useState } from 'react'
-import { io } from 'socket.io-client'
-import type { Socket } from 'socket.io-client'
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import type { Socket } from "socket.io-client";
 
-const SOCKET_SERVER_URL = 'http://localhost:3000'
+const SOCKET_SERVER_URL = import.meta.env.VITE_BACKEND_URL;
 
 interface UseSocketResult {
-  socket: Socket | null
-  message: string
+  socket: Socket | null;
+  message: string;
 }
 
 export const useSocket = (): UseSocketResult => {
-  const [socket, setSocket] = useState<Socket | null>(null)
-  const [message, setMessage] = useState<string>('')
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const [message, setMessage] = useState<string>("");
 
   useEffect(() => {
-    const newSocket: Socket = io(SOCKET_SERVER_URL)
-    setSocket(newSocket)
+    const newSocket: Socket = io(SOCKET_SERVER_URL);
+    setSocket(newSocket);
 
-    newSocket.on('connect', () => {
-      console.log(`Conectado con ID: ${newSocket.id}`)
+    newSocket.on("connect", () => {
+      console.log(`Conectado con ID: ${newSocket.id}`);
       //newSocket.emit('exampleEvent', {
       //  msg: 'Client from React was connected',
       //})
-    })
+    });
 
-    newSocket.on('project-status', (data) => {
+    newSocket.on("project-status", (data) => {
       //console.log(`Respuesta del servidor status: ${data}`);
-      setMessage(data)
-    })
+      setMessage(data);
+    });
 
     // clear conexion
     return () => {
-      newSocket.close()
-    }
-  }, [])
+      newSocket.close();
+    };
+  }, []);
 
-  return { socket, message }
-}
+  return { socket, message };
+};
