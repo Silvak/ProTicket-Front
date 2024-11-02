8c61d703cae858f7abc997464aa77e90169debac
silvak <silvak.jeg@gmail.com>
2024-11-02 17:46:35 -0400

    feat: teslo unauthorized interceptors

--- src/api/teslo.ts
+++ src/api/teslo.ts
@@ -1,4 +1,4 @@
-import { useAuthStore } from '@/store'
+import { useAuthStore, useHistoryStore, useProjectStore, useTicketStore, useUserStore } from '@/store'
 import axios from 'axios'
 
 const tesloApi = axios.create({
@@ -17,5 +17,31 @@ tesloApi.interceptors.request.use((config) => {
 
   return config
 })
+
+// Interceptor de respuesta para manejar expiración del JWT
+tesloApi.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const authStore = useAuthStore.getState()
+    const cleanHistoryData = useHistoryStore.getState().cleanHistoryData
+    const cleanTicketsData = useTicketStore.getState().cleanTicketsData
+    const cleaProjectData = useProjectStore.getState().cleanProjectData
+    const cleanUserData = useUserStore.getState().cleanUserData
+
+    if (error.response && error.response.status === 401) {
+      // Si el error es 401, el token ha expirado o es inválido
+
+      authStore.logoutUser() // Llamar al método de logout en la store
+      cleanHistoryData() // Limpiar el store de historias
+      cleanTicketsData() // Limpiar el store de tickets
+      cleaProjectData() // Limpiar el store de proyectos
+      cleanUserData() // Limpiar el store de usuarios
+      // Puedes redirigir al usuario a la página de login si es necesario
+      window.location.href = '/login'
+    }
+
+    return Promise.reject(error)
+  }
+)
 
 export { tesloApi }
